654c58be00c769a7deb88fbe27687af2eec3c623
Roman.Golovanov <Roman.Golovanov@5f731c50-f1f8-11de-b2c3-d77de7315633>
2010-01-07 22:26:58 +0000

    added scroll bars

--- workspace/aMetro/src/com/ametro/BrowseLibrary.java
+++ workspace/aMetro/src/com/ametro/BrowseLibrary.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.ExpandableListView;
