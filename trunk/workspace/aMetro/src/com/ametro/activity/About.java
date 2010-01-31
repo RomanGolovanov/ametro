2f2967ac3c28c1d3120789b6acfdc8b4b8095221
Roman.Golovanov <Roman.Golovanov@5f731c50-f1f8-11de-b2c3-d77de7315633>
2010-01-31 17:00:47 +0000

    added cleanup funciton in  maps>import activity

--- trunk/workspace/aMetro/src/com/ametro/activity/About.java
+++ trunk/workspace/aMetro/src/com/ametro/activity/About.java
@@ -3,7 +3,11 @@ package com.ametro.activity;
 import com.ametro.R;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class About extends Activity {
 	/**
@@ -13,5 +17,21 @@ public class About extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
+
+        try {
+			PackageManager manager = getPackageManager();
+			PackageInfo info = manager.getPackageInfo(getPackageName(), 0);
+			String versionName = info.versionName;
+			TextView versionText = (TextView)findViewById(R.id.about_version_text);
+			versionText.setText(
+				String.format(
+						"%s %s",
+						getString(R.string.app_name),
+						versionName
+						));
+		} catch (NameNotFoundException e) {
+			finish();
+		}
+        
 	}
 }
