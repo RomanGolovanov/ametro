4617d5b183f9142c2cbd5bee7fa59e7328190c84
Roman.Golovanov <Roman.Golovanov@5f731c50-f1f8-11de-b2c3-d77de7315633>
2010-03-13 19:40:38 +0000

    added context menu for favorite routes list

--- trunk/workspace/aMetro/src/org/ametro/activity/FavoriteRouteList.java
+++ trunk/workspace/aMetro/src/org/ametro/activity/FavoriteRouteList.java
@@ -23,6 +23,7 @@ package org.ametro.activity;
 import static org.ametro.Constants.STATION_FROM_ID;
 import static org.ametro.Constants.STATION_TO_ID;
 
+import org.ametro.R;
 import org.ametro.adapter.RouteFavoriteListAdapter;
 import org.ametro.model.SubwayMap;
 
@@ -30,30 +31,76 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class FavoriteRouteList extends ListActivity {
 
 	private Point[] mRoutes;
 	private SubwayMap mSubwayMap;
 	
+	private static final int CONTEXT_MENU_SELECT = 0;
+	private static final int CONTEXT_MENU_REMOVE = 1;
+	
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mRoutes = BrowseVectorMap.Instance.getFavoriteRoutes();
-		mSubwayMap = BrowseVectorMap.Instance.getSubwayMap();
-		
-		final RouteFavoriteListAdapter adapter = new RouteFavoriteListAdapter(this, mRoutes, mSubwayMap);
-		setListAdapter(adapter);		
+		onBindData();
+		registerForContextMenu(getListView());
+	}
+
+
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		menu.add(ContextMenu.NONE, CONTEXT_MENU_SELECT, CONTEXT_MENU_SELECT, R.string.menu_select);
+		menu.add(ContextMenu.NONE, CONTEXT_MENU_REMOVE, CONTEXT_MENU_REMOVE, R.string.menu_remove);
+		super.onCreateContextMenu(menu, v, menuInfo);
+	}
+	
+	public boolean onContextItemSelected(MenuItem item) {
+		switch(item.getItemId()){
+		case CONTEXT_MENU_SELECT:
+			onSelect((int)getSelectedItemId());
+			return true;
+		case CONTEXT_MENU_REMOVE:
+			onRemove((int)getSelectedItemId());
+			return true;
+		}
+		return super.onContextItemSelected(item);
 	}
 	
 	protected void onListItemClick(ListView l, View v, int position, long id) {
+		onSelect(position);
+		super.onListItemClick(l, v, position, id);
+	}
+
+	private void onSelect(int position) {
 		Intent data = new Intent();
 		Point r = mRoutes[position];
 		data.putExtra(STATION_FROM_ID, r.x);
 		data.putExtra(STATION_TO_ID, r.y);
 		setResult(RESULT_OK, data);
 		finish();
-		super.onListItemClick(l, v, position, id);
 	}
+	
+	private void onRemove(int position){
+		Point p = mRoutes[position];
+		BrowseVectorMap.Instance.removeFavoriteRoute(p.x, p.y);
+		onBindData();
+	}
+	
+	private void onBindData() {
+		mRoutes = BrowseVectorMap.Instance.getFavoriteRoutes();
+		if(mRoutes==null || mRoutes.length == 0){
+			Toast.makeText(this, R.string.msg_no_favorites, Toast.LENGTH_SHORT).show();
+			finish();
+		}
+		mSubwayMap = BrowseVectorMap.Instance.getSubwayMap();
+		final RouteFavoriteListAdapter adapter = new RouteFavoriteListAdapter(this, mRoutes, mSubwayMap);
+		setListAdapter(adapter);
+	}
+		
 }
