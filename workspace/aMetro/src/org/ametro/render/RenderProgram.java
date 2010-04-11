909994b0ff7f2d8ea3b35c7794c59e5550fa9415
Roman.Golovanov <Roman.Golovanov@5f731c50-f1f8-11de-b2c3-d77de7315633>
2010-04-11 07:55:02 +0000

    rewrited grayed objects rendering - using color recalculation instead alfa-channel

--- workspace/aMetro/src/org/ametro/render/RenderProgram.java
+++ workspace/aMetro/src/org/ametro/render/RenderProgram.java
@@ -293,11 +293,17 @@ public class RenderProgram {
 		if(color == Color.BLACK){
 			return 0xFFd0d0d0;
 		}
-		float[] hsv = new float[3]; 
-		Color.colorToHSV(color, hsv);
-		hsv[1] = 0.1f;
-		hsv[2] = 0.9f;
-		return Color.HSVToColor(0xFF, hsv);
+
+		float r = (float)Color.red(color) / 255;
+		float g = (float)Color.green(color) / 255;
+		float b = (float)Color.blue(color) / 255;
+		
+		float t = 0.8f;
+		r = r*(1-t) + 1.0f * t;
+		g = g*(1-t) + 1.0f * t;
+		b = b*(1-t) + 1.0f * t;
+		 
+		return Color.argb(0xFF, (int)Math.min(r * 255,255), (int)Math.min(g * 255,255), (int)Math.min(b * 255,255));
 	}
 	
 }
