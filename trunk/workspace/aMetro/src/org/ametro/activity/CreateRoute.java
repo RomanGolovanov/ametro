984b201fd24688fd10a4714a5147b651c18d1ba2
roman.golovanov <roman.golovanov@5f731c50-f1f8-11de-b2c3-d77de7315633>
2010-05-23 14:03:48 +0000

    fixed scheme changing force-close while route is selected, maps became incompatible with previous version

--- trunk/workspace/aMetro/src/org/ametro/activity/CreateRoute.java
+++ trunk/workspace/aMetro/src/org/ametro/activity/CreateRoute.java
@@ -34,7 +34,6 @@ import org.ametro.model.route.RouteBuilder;
 import org.ametro.model.route.RouteContainer;
 import org.ametro.model.route.RouteParameters;
 import org.ametro.util.DateUtil;
-import org.ametro.util.StringUtil;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -140,7 +139,7 @@ public class CreateRoute extends Activity implements OnClickListener,
 		
 		mMapView = BrowseVectorMap.Instance.getMapView();
 
-		mTransports = mMapView.getTransportCollection();
+		mTransports = mMapView.getTransportCollection(this);
 		
 		StationListAdapter fromAdapter = new StationListAdapter(this, mMapView.stations, mMapView); 
 		StationListAdapter toAdapter = new StationListAdapter(this, mMapView.stations, mMapView); 
@@ -246,7 +245,7 @@ public class CreateRoute extends Activity implements OnClickListener,
 		builder.setTitle(R.string.msg_select_transports);
 		final TransportCollection coll = new TransportCollection(mTransports);
 		
-		builder.setMultiChoiceItems(StringUtil.getResourceStringArray(this,coll.getNames()) , coll.getStates(), new DialogInterface.OnMultiChoiceClickListener() {
+		builder.setMultiChoiceItems(coll.getNames() , coll.getStates(), new DialogInterface.OnMultiChoiceClickListener() {
 			public void onClick(DialogInterface dialog, int which, boolean isChecked) {
 				coll.setState(which, isChecked);
 			}
