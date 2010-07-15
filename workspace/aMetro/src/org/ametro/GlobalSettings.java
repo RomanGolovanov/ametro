cb1f2093a82bcbac7012fd38895b4ce753b083d3
Roman.Golovanov <Roman.Golovanov@5f731c50-f1f8-11de-b2c3-d77de7315633>
2010-07-15 15:40:44 +0000

    added online map sync timestamp setting in GlobalSettings

--- workspace/aMetro/src/org/ametro/GlobalSettings.java
+++ workspace/aMetro/src/org/ametro/GlobalSettings.java
@@ -21,17 +21,18 @@
 
 package org.ametro;
 
-import static org.ametro.Constants.IMPORT_FILE_TYPE;
 import static org.ametro.Constants.DOWNLOAD_FILE_TYPE;
+import static org.ametro.Constants.IMPORT_FILE_TYPE;
 import static org.ametro.Constants.LOCAL_CATALOG_PATH;
 import static org.ametro.Constants.MAP_FILE_TYPE;
 import static org.ametro.Constants.PREFERENCE_AUTO_UPDATE_INDEX;
 import static org.ametro.Constants.PREFERENCE_AUTO_UPDATE_MAPS;
 import static org.ametro.Constants.PREFERENCE_DEBUG;
-import static org.ametro.Constants.PREFERENCE_LOCALE;
-import static org.ametro.Constants.TEMP_CATALOG_PATH;
-import static org.ametro.Constants.PREFERENCE_PMZ_IMPORT;
 import static org.ametro.Constants.PREFERENCE_ENABLE_COUNTRY_ICONS;
+import static org.ametro.Constants.PREFERENCE_LOCALE;
+import static org.ametro.Constants.PREFERENCE_ONLINE_CATALOG_UPDATE_DATE;
+import static org.ametro.Constants.PREFERENCE_PMZ_IMPORT;
+import static org.ametro.Constants.TEMP_CATALOG_PATH;
 
 import java.io.File;
 import java.util.Locale;
@@ -56,7 +57,7 @@ public class GlobalSettings {
 			return code;
 		}
 	}
-    
+	
     public static String getLocalCatalogMapFileName(String systemName) {
         return new File(LOCAL_CATALOG_PATH, systemName).getAbsolutePath().toLowerCase();
     }
@@ -84,6 +85,16 @@ public class GlobalSettings {
 		editor.commit();
 	}
 
+	public static long getOnlineCatalogUpdateDate(Context context) {
+		return PreferenceManager.getDefaultSharedPreferences(context).getLong(PREFERENCE_ONLINE_CATALOG_UPDATE_DATE, 0);
+	}
+
+	public static void setOnlineCatalogUpdateDate(Context context, long timestamp) {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		Editor editor = prefs.edit();
+		editor.putLong(PREFERENCE_ONLINE_CATALOG_UPDATE_DATE, timestamp);
+		editor.commit();
+	}
 	
 	public static boolean isDebugMessagesEnabled(Context context) {
 		return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(PREFERENCE_DEBUG, false);
