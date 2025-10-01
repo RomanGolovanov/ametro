ef35f6072ac46b8905d38af439d1a2d09f1e4c9e
Roman Golovanov <rvg@syncmd.com>
2025-10-01 14:49:19 +0100

    tune gradle configs for F-Droid

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,11 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.13.0"
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
